ceb7e40210f04eeed46dabadc7f1da015dd71248
24LP5569 24LP5569 <24LP5569@G304-CC0232.local>
2024-12-24 09:09:24 +0800

    header merchant

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       hello world
+      <Header />
       <div></div>
     </div>
   );
